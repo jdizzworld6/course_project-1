81b5338c13bade276762e474b42b1fbccb7ad084
Jeremy Hansen <jeremy.j.hansen3420@gmail.com>
2025-04-04 15:23:57 -0400

    activate app

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
+import React from 'react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  test: {
-    environment: 'jsdom', // or 'happy-dom'
-    globals: true,
-    css: true,
-    setupFiles: ['./src/tests/setup.js'], // Add this line
-  },
+  // test: {
+  //   environment: 'jsdom', // or 'happy-dom'
+  //   globals: true,
+  //   css: true,
+  //   setupFiles: ['./src/tests/setup.js'], // Add this line
+  // },
+  plugins: [react()]
 })
