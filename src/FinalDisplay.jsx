a860e1e5174520262f0bddb9c74a39432d312818
Jeremy Hansen <jeremy.j.hansen3420@gmail.com>
2025-04-05 11:00:57 -0400

    finished styling

--- src/FinalDisplay.jsx
+++ src/FinalDisplay.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import PickerPage from "./PickerPage";
+import "./FinalDisplay.css"
 import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from "recharts";
 
 
 export default function FinalDisplay() {
     const [displayData, setDisplayData] = useState([]);
+
     const [countItems, setCountItems] = useState([
         { type: "Jacket", count: 0 },
         { type: "Shirt", count: 0 },
@@ -36,6 +38,7 @@ export default function FinalDisplay() {
             )
         );
     }
+
     // add or updates final display
     const handleDisplayDataChange = (newData) => {
         const changeData = newData[0]
@@ -48,13 +51,14 @@ export default function FinalDisplay() {
                 prevImageData.map((image) =>
                     image.id === changeData.id ? { ...image, note: changeData.note } : image)
             );
+
             // adds to final display
         } else {
             setDisplayData([...displayData, changeData]);
         }
 
     };
-
+    // Clears photos
     const clearPhotos = () => {
         setDisplayData([]);
     }
@@ -62,38 +66,46 @@ export default function FinalDisplay() {
     return <>
 
 
-        <BarChart width={730} height={250} data={countItems}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="type" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="count" fill="#8884d8" />
-        </BarChart>
-        <h1>Hello World</h1>
-
-        <div className="container">
-            <div className="pickingItems">
-                <PickerPage suiteCategory={"Jacket"} onDataChange={handleDisplayDataChange} onUploadItem={handleCount} onDeleteItem={removeCount} onCountFinal={updateFinalCount} />
-                <PickerPage suiteCategory={"Shirt"} onDataChange={handleDisplayDataChange} onUploadItem={handleCount} onDeleteItem={removeCount} onCountFinal={updateFinalCount} />
-                <PickerPage suiteCategory={"Tie"} onDataChange={handleDisplayDataChange} onUploadItem={handleCount} onDeleteItem={removeCount} onCountFinal={updateFinalCount} />
-                <PickerPage suiteCategory={"Shoes"} onDataChange={handleDisplayDataChange} onUploadItem={handleCount} onDeleteItem={removeCount} onCountFinal={updateFinalCount} />
-
-            </div>
-
-            <div className="finalDisplay">
-                <h2>Final Display</h2>
-
-                <div className='suiteHolder'>
-                    {displayData.map((image) =>
-                        <div className='img' key={image.id}>
-                            <img className='imgMain' src={image.url}></img>
-                            <h3>{image.note}</h3>
-                        </div>
-                    )}
+        <div>
+            <div className="container">
+                <div className="item-1">
+                    <BarChart id="barchart" width={730} height={250} data={countItems}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="type" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar id="mybar" dataKey="count" fill="#8884d8" />
+                    </BarChart>
                 </div>
-                <button onClick={clearPhotos}>Clear</button>
+                <div className="item-2">
+                    <img id="titleImage" src="public\images\titleImage.png"></img>
+                </div>
             </div>
+        </div>
+        <div className="grid-container">
+
+            <div className="content">
+                <PickerPage suiteCategory={"Jacket"} onDataChange={handleDisplayDataChange} onUploadItem={handleCount} onDeleteItem={removeCount} onCountFinal={updateFinalCount} />
+
+                <PickerPage suiteCategory={"Shirt"} onDataChange={handleDisplayDataChange} onUploadItem={handleCount} onDeleteItem={removeCount} onCountFinal={updateFinalCount} />
+
+                <PickerPage suiteCategory={"Tie"} onDataChange={handleDisplayDataChange} onUploadItem={handleCount} onDeleteItem={removeCount} onCountFinal={updateFinalCount} />
+
+                <PickerPage suiteCategory={"Shoes"} onDataChange={handleDisplayDataChange} onUploadItem={handleCount} onDeleteItem={removeCount} onCountFinal={updateFinalCount} />
+            </div>
+
+            <div className="right-nav">
+                <h2 id="title">Final Display</h2>
+                {displayData.map((image) =>
+                    <div className='img' key={image.id}>
+                        <img className='imgMain' src={image.url}></img>
+                        <h3>{image.note}</h3>
+                    </div>
+                )}
+                <button id="clear" onClick={clearPhotos}>Clear</button>
+            </div>
+
         </div>
 
     </>
